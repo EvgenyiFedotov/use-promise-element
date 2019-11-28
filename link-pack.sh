55a38be2f2a0f1c7d4e8e46c49986989966c7cbb
e.fedotov <fedotov1290@gmail.com>
2019-11-28 21:13:47 +0300

    add minify package

--- link-pack.sh
+++ link-pack.sh
@@ -3,7 +3,6 @@
 FILE_NAME="package-pack.tgz"
 PACK_NAME="use-node-promise-pack"
 
-yarn build
 yarn pack --filename ${FILE_NAME}
 mkdir -pv ../${PACK_NAME}
 tar -xvzf ${FILE_NAME} -C ../${PACK_NAME}
