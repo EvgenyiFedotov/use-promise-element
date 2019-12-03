e02e303e7be9fa0d87964f2f56a06eed0437e06b
e.fedotov <fedotov1290@gmail.com>
2019-12-03 17:17:15 +0300

    add tests

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -1,11 +1,76 @@
+import "@babel/polyfill";
 import * as React from "react";
-import { mount } from "enzyme";
+import * as enzyme from "enzyme";
+import ReactTestUtils from "react-dom/test-utils";
+import ReactDOM from "react-dom";
 
-const Modal = () => {
-  return <></>;
+import { useNodePromise } from "../src";
+
+interface ModalProps {
+  onClose?(e: React.MouseEvent<HTMLDivElement>): void;
+  onError?(): void;
+}
+
+const Modal: React.FC<ModalProps> = (props) => {
+  return (
+    <div className="modal" onClick={props.onClose}>
+      modal
+    </div>
+  );
 };
 
-test("main", () => {
-  const wrapper = mount(<Modal />);
-  console.log(wrapper);
+const modalCreateProps = (resolve, reject) => ({
+  onClose: (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    resolve();
+  },
+  onError: () => {
+    reject();
+  },
+});
+
+const App: React.FC = (props) => {
+  const [modal, create] = useNodePromise(Modal, modalCreateProps);
+
+  const onClick = React.useCallback(() => {
+    create();
+  }, [create]);
+
+  return <div onClick={onClick}>{modal}</div>;
+};
+
+describe("html app", () => {
+  test.skip("without modal", () => {
+    const app = enzyme.mount(<App />);
+
+    expect(app.html()).toBe("<div></div>");
+  });
+
+  test.skip("with modal", async () => {
+    const app = enzyme.mount(<App />);
+
+    app.simulate("click");
+
+    expect(app.find(Modal)).toHaveLength(1);
+  });
+
+  test("open modal and close", async () => {
+    const app = enzyme.mount(<App />);
+
+    ReactTestUtils.act(() => {
+      app.simulate("click");
+    });
+
+    app.update();
+
+    ReactTestUtils.act(() => {
+      app.find(Modal).simulate("click");
+    });
+
+    app.update();
+
+    ReactTestUtils.act(() => {
+      console.log(app.find(Modal).length);
+    });
+  });
 });
