56704bb5367c7c5cba3fd696a74c596809205af6
e.fedotov <fedotov1290@gmail.com>
2019-12-07 15:33:50 +0300

    fix: add types, add method 'close', renames

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,92 @@
 import * as React from "react";
 
-export const useNodePromise = <R = any, P = {}>(
-  component: React.ComponentClass | React.FC,
-  getProps?: GetProps<R, P>,
-): Result<R, P> => {
-  const [value, setValue] = React.useState<React.ReactElement>(
-    React.createElement(React.Fragment),
-  );
+export type GetProps<Result = any, Props extends object = {}> = (
+  resolve: (value?: Result | PromiseLike<Result> | undefined) => void,
+  reject: (reason?: any) => void,
+) => Props;
 
-  const [actionResult, setActionResult] = React.useState<Promise<R> | null>(
+export type DefaultProps = GetProps<
+  any,
+  {
+    onSuccess(): void;
+    onCancel(): void;
+  }
+>;
+
+type Open<Result = any, Props extends object = DefaultProps> = (
+  getProps?: GetProps<Result, Props>,
+) => Promise<Result>;
+
+type Close = () => void;
+
+export type UseNodePromise<
+  Result = any,
+  Props extends object = DefaultProps
+> = [React.ReactElement | null, Open<Result, Props>, Close];
+
+export const useNodePromise = <
+  Result = any,
+  Props extends object = DefaultProps
+>(
+  component: React.ComponentClass<Props | object> | React.FC<Props | object>,
+  getProps?: GetProps<Result, Props>,
+): UseNodePromise<Result, Props> => {
+  const [element, setElement] = React.useState<React.ReactElement<
+    Props | object
+  > | null>(null);
+
+  const [openResult, setOpenResult] = React.useState<Promise<Result> | null>(
     null,
   );
 
-  const action = React.useCallback(
-    (getPropsAction?: GetProps<R, P>) => {
-      const result = new Promise<R>((resolve, reject) => {
-        const nextValue = React.createElement(component, {
+  const open = React.useCallback(
+    (getPropsAction?: GetProps<Result, Props>) => {
+      const result = new Promise<Result>((resolve, reject) => {
+        let props: Props | object = {
           ...(getProps && getProps(resolve, reject)),
           ...(getPropsAction && getPropsAction(resolve, reject)),
-        });
+        };
 
-        setValue(nextValue);
+        if (!Object.keys(props).length) {
+          props = {
+            onSuccess: resolve,
+            onCancel: reject,
+          };
+        }
+
+        const nextValue = React.createElement<Props | object>(component, props);
+
+        setElement(nextValue);
       });
 
-      setActionResult(result);
+      setOpenResult(result);
 
       return result;
     },
     [component, getProps],
   );
 
+  const close = React.useCallback(() => {
+    setElement(null);
+  }, []);
+
   React.useEffect(() => {
     let mount = true;
 
-    if (actionResult) {
-      actionResult.finally(() => {
-        if (mount) {
-          setValue(React.createElement(React.Fragment));
-        }
-      });
+    if (openResult) {
+      openResult
+        .catch((er) => er)
+        .finally(() => {
+          if (mount) {
+            close();
+          }
+        });
     }
 
     return () => {
       mount = false;
     };
-  }, [actionResult]);
+  }, [openResult]);
 
-  return [value, action];
+  return [element, open, close];
 };
-
-export type GetProps<R = any, P = {}> = (
-  resolve: (value?: R | PromiseLike<R> | undefined) => void,
-  reject: (reason?: any) => void,
-) => P;
-
-export type Result<R = any, P = {}> = [
-  React.ReactElement,
-  (resolveReject?: GetProps<R, P>) => Promise<R>,
-];
