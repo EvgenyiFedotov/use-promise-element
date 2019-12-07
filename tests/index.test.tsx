027074de5800bc01eda7990523c69bb7ffaf5c84
e.fedotov <fedotov1290@gmail.com>
2019-12-07 15:35:24 +0300

    tests: add other tests

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -3,76 +3,148 @@ import * as React from "react";
 import * as enzyme from "enzyme";
 import ReactTestUtils from "react-dom/test-utils";
 
-import { useNodePromise, GetProps } from "../src";
+import { Modal } from "./components/modal";
+import { App, Confirm } from "./components/app";
 
-type ModalResolve = "success" | "cancel";
+let app: enzyme.ReactWrapper;
 
-interface ModalProps {
-  onClose?(e: React.MouseEvent<HTMLDivElement>): void;
-  onError?(): void;
-}
-
-const Modal: React.FC<ModalProps> = (props) => {
-  return (
-    <div className="modal" onClick={props.onClose}>
-      modal
-    </div>
-  );
-};
-
-const modalCreateProps: GetProps<ModalResolve, ModalProps> = (
-  resolve,
-  reject,
-) => ({
-  onClose: (e: React.MouseEvent<HTMLDivElement>) => {
-    e.stopPropagation();
-    resolve();
-  },
-  onError: () => {
-    reject();
-  },
-});
-
-const App: React.FC = (props) => {
-  const [modal, create] = useNodePromise(Modal, modalCreateProps);
-
-  const onClick = React.useCallback(() => {
-    create();
-  }, [create]);
-
-  return <div onClick={onClick}>{modal}</div>;
-};
-
-describe("html app", () => {
-  test("without modal", () => {
-    const app = enzyme.mount(<App />);
-
-    expect(app.html()).toBe("<div></div>");
+const simulate = async (wrapper: enzyme.ReactWrapper, nameEvent: string) => {
+  await ReactTestUtils.act(async () => {
+    wrapper.simulate(nameEvent);
   });
 
-  test("with modal", async () => {
-    const app = enzyme.mount(<App />);
+  app.update();
+};
 
-    app.simulate("click");
+beforeEach(() => {
+  app = enzyme.mount(<App />);
+});
 
+describe("modal didn't open", () => {
+  test("exist buttons", () => {
+    expect(app.find("button")).toHaveLength(4);
+  });
+
+  test("result app", () => {
+    expect(app.find(".app-result").text()).toBe("");
+  });
+
+  test("result modal", () => {
+    expect(app.find(Modal)).toHaveLength(0);
+  });
+});
+
+describe("open modal", () => {
+  beforeEach(async () => {
+    await simulate(app.find(".app-open"), "click");
+  });
+
+  test("exist modal", async () => {
     expect(app.find(Modal)).toHaveLength(1);
   });
 
-  test.skip("open modal and close", async () => {
-    const app = enzyme.mount(<App />);
+  describe("close after resolve", () => {
+    const close = async (selectorButton: string) => {
+      await simulate(app.find(Modal).find(selectorButton), "click");
+    };
 
-    ReactTestUtils.act(() => {
-      app.simulate("click");
+    describe("success", () => {
+      beforeEach(async () => {
+        await close(".modal-success");
+      });
+
+      test("exist modal", () => {
+        expect(app.find(Modal)).toHaveLength(0);
+      });
+
+      test("result app", () => {
+        expect(app.find(".app-result").text()).toBe("success");
+      });
     });
 
-    app.update();
+    describe("cancel", () => {
+      beforeEach(async () => {
+        await close(".modal-close");
+      });
 
-    ReactTestUtils.act(() => {
-      app.find(Modal).simulate("click");
+      test("exist modal", () => {
+        expect(app.find(Modal)).toHaveLength(0);
+      });
+
+      test("result app", () => {
+        expect(app.find(".app-result").text()).toBe("cancel");
+      });
+    });
+  });
+
+  describe("close after run method 'close'", () => {
+    beforeEach(async () => {
+      await simulate(app.find(".app-close"), "click");
     });
 
-    app.update();
+    test("exist modal", () => {
+      expect(app.find(Modal)).toHaveLength(0);
+    });
 
-    expect(app.find(Modal)).toHaveLength(0);
+    test("result app", () => {
+      expect(app.find(".app-result").text()).toBe("");
+    });
+  });
+
+  describe("close after error", () => {
+    beforeEach(async () => {
+      await simulate(app.find(Modal).find(".modal-error"), "click");
+    });
+
+    test("exist modal", () => {
+      expect(app.find(Modal)).toHaveLength(0);
+    });
+
+    test("result app", () => {
+      expect(app.find(".app-result").text()).toBe("error");
+    });
+  });
+});
+
+describe("open modal with props", () => {
+  beforeEach(async () => {
+    await simulate(app.find(".app-open-with-props"), "click");
+  });
+
+  test("exist modal", () => {
+    expect(app.find(Modal)).toHaveLength(1);
+  });
+
+  test("equal title", () => {
+    expect(
+      app
+        .find(Modal)
+        .find(".modal-title")
+        .text(),
+    ).toBe("Confirm title");
+  });
+});
+
+describe("open modal confirm without props", () => {
+  beforeEach(async () => {
+    await simulate(app.find(".app-open-confirm"), "click");
+  });
+
+  test("exist modal", () => {
+    expect(app.find(Confirm)).toHaveLength(1);
+  });
+
+  describe("close", () => {
+    beforeEach(async () => {
+      await simulate(app.find(Confirm).find(".modal-success"), "click");
+    });
+
+    test("exist modal", () => {
+      expect(app.find(Confirm)).toHaveLength(0);
+    });
+
+    test("result app", () => {
+      expect(app.find(".app-result").text()).toBe("success");
+    });
   });
 });
