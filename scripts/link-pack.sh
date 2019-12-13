9bad5813719d244af90de131f203bc662c36c0cd
e.fedotov <fedotov1290@gmail.com>
2019-12-13 11:56:47 +0300

    setup: fix script, use name package

--- scripts/link-pack.sh
+++ scripts/link-pack.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 FILE_NAME="package-pack.tgz"
-PACK_NAME="use-promise-element-pack"
+PACKAGE_NAME=$(cat package.json | grep name | head -1 | awk -F: '{ print $2 }' | sed 's/[ ",]//g')
+PACKAGE_LINK="$PACKAGE_NAME-pack"
 
-yarn pack --filename ${FILE_NAME}
-mkdir -pv ../${PACK_NAME}
-tar -xvzf ${FILE_NAME} -C ../${PACK_NAME}
-rm ${FILE_NAME}
-cd ../${PACK_NAME}/package
+yarn pack --filename $FILE_NAME
+mkdir -pv ../$PACKAGE_LINK
+tar -xvzf $FILE_NAME -C ../$PACKAGE_LINK
+rm $FILE_NAME
+cd ../$PACKAGE_LINK/package
 yarn link
--- scripts/unlink-pack.sh
+++ scripts/unlink-pack.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-PACK_NAME="use-promise-element-pack"
+PACKAGE_NAME=$(cat package.json | grep name | head -1 | awk -F: '{ print $2 }' | sed 's/[ ",]//g')
+PACKAGE_LINK="$PACKAGE_NAME-pack"
 
-cd ../${PACK_NAME}/package
+cd ../$PACKAGE_LINK/package
 yarn unlink
-cd ../..
-rm -fr ${PACK_NAME}
+rm -fr ../../$PACKAGE_LINK
