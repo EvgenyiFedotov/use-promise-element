396de6b2d2585f36128776fb811595eadbecd7f8
e.fedotov <fedotov1290@gmail.com>
2019-12-15 22:48:54 +0300

    scripts: setup publish storybook

--- scripts/publish-storybook.sh
+++ scripts/publish-storybook.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
 yarn build-storybook
-mkdir package
-mv ./(.*|*) package 2>/dev/null || :
-mv ./package/storybook-static ./
-# rm -fr package
+
+mkdir -p package
+mv .* ./package 2>/dev/null || :
+mv * ./package 2>/dev/null || :
+mv ./package/.git ./  2>/dev/null || :
+mv ./package/storybook-static/* ./ 2>/dev/null || :
+rm -fr package
+
+git push origin :gh-pages 2>/dev/null || :
+git branch -D gh-pages 2>/dev/null || :
+git checkout -b gh-pages
+git add -A
+git commit -m "[storybook] update"
+git push -u -f origin gh-pages
